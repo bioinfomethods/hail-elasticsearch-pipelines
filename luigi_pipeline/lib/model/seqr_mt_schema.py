8c18ac29af838764e00aa5726958ac7c42218932
Tommy Li <tommy.li@mcri.edu.au>
2020-12-17 16:54:10 +1100

    feat: Implemented Genetale annotations to luigi Seqr load
    (cherry picked from commit 955ca94e18a3784dc72a7156465bf9db46610d1e)

--- luigi_pipeline/lib/model/seqr_mt_schema.py
+++ luigi_pipeline/lib/model/seqr_mt_schema.py
@@ -1,6 +1,7 @@
 import hail as hl
 
 from lib.model.base_mt_schema import BaseMTSchema, row_annotation, RowAnnotationOmit
+from lib.model.mcri_seqr_mt_schema import SeqrGenetaleSchema
 from hail_scripts.computed_fields import variant_id
 from hail_scripts.computed_fields import vep
 
@@ -284,7 +285,7 @@ class SeqrGenotypesSchema(BaseMTSchema):
         }
 
 
-class SeqrVariantsAndGenotypesSchema(SeqrVariantSchema, SeqrGenotypesSchema):
+class SeqrVariantsAndGenotypesSchema(SeqrGenetaleSchema, SeqrVariantSchema, SeqrGenotypesSchema):
     """
     Combined variant and genotypes.
     """
