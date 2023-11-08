ab5e93d349a2646f3c05ac7bca93a2e46ed6d10f
tommy.li <tommy.li@firefire.co>
2023-11-08 20:58:47 +1100

    refactor: Rename hep to slp

--- mcri_deploy/scripts/build_pr.sh
+++ mcri_deploy/scripts/build_pr.sh
@@ -35,7 +35,6 @@ check_command() {
 
 check_command "git"
 check_command "docker"
-check_command "docker-compose"
 
 if [ -z "$1" ]; then
     usage
@@ -64,7 +63,7 @@ build() {
     COMPOSE_FILE="$PROJECT_DIR/mcri_deploy/docker-compose/docker-compose.yml"
     COMPOSE_BUILD_FILE="$PROJECT_DIR/mcri_deploy/docker-compose/docker-compose.build.yml"
 
-    cp "$PROJECT_DIR/mcri_deploy/docker-compose/hep.template.env" "$PROJECT_DIR/.env" 
+    cp "$PROJECT_DIR/mcri_deploy/docker-compose/slp.template.env" "$PROJECT_DIR/.env"
     COMPOSE_ENV_FILE="$PROJECT_DIR/.env"
     
     for line in $(grep -v '^#' "$PROJECT_DIR/.env" | xargs)
@@ -73,9 +72,9 @@ build() {
     done
 
     # Tagging
-    HEP_GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
-    HEP_GIT_BRANCH_TAG=${HEP_GIT_BRANCH_NAME/\//_}
-    HEP_LONG_GIT_TAG=$(git describe --long --always)
+    SLP_GIT_BRANCH_NAME=$(git rev-parse --abbrev-ref HEAD)
+    SLP_GIT_BRANCH_TAG=${SLP_GIT_BRANCH_NAME/\//_}
+    SLP_LONG_GIT_TAG=$(git describe --long --always)
 
     build_pr "$@"
 }
@@ -83,37 +82,37 @@ build() {
 build_pr() {
     info "Building pipeline-runner component only"
     set -x
-    docker-compose --verbose \
+    docker compose --verbose \
       -f "$COMPOSE_FILE" \
       -f "$COMPOSE_BUILD_FILE" \
       --env-file="$COMPOSE_ENV_FILE" \
       build $BUILD_OPTS pipeline-runner
 
-    docker-compose --verbose \
+    docker compose --verbose \
       -f "$COMPOSE_FILE" \
       -f "$COMPOSE_BUILD_FILE" \
       --env-file="$COMPOSE_ENV_FILE" \
       push pipeline-runner
 
-    docker tag "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$PIPELINE_RUNNER_IMAGE_TAG" "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$HEP_GIT_BRANCH_TAG"
-    docker push "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$HEP_GIT_BRANCH_TAG"
+    docker tag "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$PIPELINE_RUNNER_IMAGE_TAG" "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SLP_GIT_BRANCH_TAG"
+    docker push "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SLP_GIT_BRANCH_TAG"
 
-    docker tag "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$PIPELINE_RUNNER_IMAGE_TAG" "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$HEP_LONG_GIT_TAG"
-    docker push "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$HEP_LONG_GIT_TAG"
+    docker tag "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$PIPELINE_RUNNER_IMAGE_TAG" "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SLP_LONG_GIT_TAG"
+    docker push "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SLP_LONG_GIT_TAG"
 
-    if [[ "$1" == "hail-elasticsearch-pipelines" ]]; then
-        HEP_VERSION="v$(date +"%Y.%m.%d")_00"
-        docker tag "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$PIPELINE_RUNNER_IMAGE_TAG" "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$HEP_VERSION"
-        docker push "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$HEP_VERSION"
+    if [[ "$1" == "seqr-loading-pipelines" ]]; then
+        SLP_VERSION="v$(date +"%Y.%m.%d")_00"
+        docker tag "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$PIPELINE_RUNNER_IMAGE_TAG" "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SLP_VERSION"
+        docker push "$CONTAINER_REGISTRY/$PIPELINE_RUNNER_IMAGE_NAME:$SLP_VERSION"
     fi
 }
 
 case "$1" in
 test)
-    build "hail-elasticsearch-pipelines-test"
+    build "seqr-loading-pipelines-test"
     ;;
 prod)
-    build "hail-elasticsearch-pipelines"
+    build "seqr-loading-pipelines"
     ;;
 *)
     usage
