d6fa86e026eed65e1ba7cfa6a48f34663bcbb977
Tommy Li <tommy.li@firefire.co>
2025-02-02 14:17:41 +1100

    feat: Remove unused Genetale annotations

--- luigi_pipeline/lib/model/seqr_mt_schema.py
+++ luigi_pipeline/lib/model/seqr_mt_schema.py
@@ -321,38 +321,6 @@ class SeqrMcriVariantSchema(BaseSeqrSchema):
     def afMcri(self):
         return self.mt.info.get('AF_MCRI', hl.missing(hl.tfloat))
 
-    @row_annotation(name='genetale_all_diseases')
-    def genetaleAllDiseases(self):
-        return self.mt.info.get('GT.All.Diseases', hl.empty_array(hl.tstr))
-
-    @row_annotation(name='genetale_all_inheritances')
-    def genetaleAllInheritances(self):
-        return self.mt.info.get('GT.All.Inheritances', hl.empty_array(hl.tstr))
-
-    @row_annotation(name='genetale_alt_res_flag')
-    def genetaleAltResFlag(self):
-        return self.mt.info.get('GT.Alt.Res.Flag', hl.empty_array(hl.tstr))
-
-    @row_annotation(name='genetale_flag')
-    def genetaleFlag(self):
-        return self.mt.info.get('GT.Flag', hl.empty_array(hl.tstr))
-
-    @row_annotation(name='genetale_gene_class_info')
-    def genetaleGeneClassInfo(self):
-        return self.mt.info.get('GT.GeneClass.Info', hl.empty_array(hl.tstr))
-
-    @row_annotation(name='genetale_gene_class')
-    def genetaleGeneClass(self):
-        return self.mt.info.get('GT.GeneClass', hl.null('str'))
-
-    @row_annotation(name='genetale_previous')
-    def genetalePrevious(self):
-        return self.mt.info.get('GT.Previous', hl.empty_array(hl.tstr))
-
-    @row_annotation(name='genetale_var_class_num')
-    def genetaleVarClassNum(self):
-        return self.mt.info.get('GT.VarClass.Num', hl.null('int'))
-
 
 class SeqrVariantsAndGenotypesSchema(SeqrVariantSchema, SeqrGenotypesSchema, SeqrMcriVariantSchema):
     """
